b0b32275ed2353a23d8614e2a63dd7443043461e
Alson van der Meulen <git@alm.nufan.net>
2022-08-21 00:24:14 +0200

    Update to new acal functions. Add range column to output.

--- ks3458a-2w-res-log.py
+++ ks3458a-2w-res-log.py
@@ -12,12 +12,12 @@ from quantiphy import Quantity
 OUTPUT_FILE = 'ks3458a-2w-res-log.csv'
 SAMPLE_INTERVAL = 10
 FIELDNAMES = ('datetime', 'dut', 'dut_setting', 'ag3458a_2_ohm', 'temp_2', 'last_acal_2',
-        'last_acal_2_cal72')
+        'last_acal_2_cal72', 'ag3458a_2_range')
 WRITE_INTERVAL_SECONDS = 3600
 STABLE_THRESHOLD = 1e-3 # Should be stable within 0.1%
 STABLE_WAIT_TIME_SECONDS = 10
 
-DEBUG = True
+DEBUG = False
 
 class State(Enum):
     WAITING = auto()
@@ -26,41 +26,30 @@ class State(Enum):
 if DEBUG:
     WRITE_INTERVAL_SECONDS = 0
 
-def start_acal_3458a_dcv(ag3458a, temp):
-    ag3458a._write('ACAL DCV')
-    ag3458a.last_acal = datetime.datetime.utcnow()
-    ag3458a.last_acal_temp = temp
-
-def start_acal_3458a_ohms(ag3458a, temp):
-    ag3458a._write('ACAL OHMS')
-
-def finish_acal_3458a(ag3458a):
-    ag3458a.last_acal_cal72 = ag3458a._ask('CAL? 72').strip()
-
-def acal_3458a(ag3458a, temp):
-    start_acal_3458a_ohms(ag3458a, temp)
-    time.sleep(12*60)
-    finish_acal_3458a(ag3458a)
+def acal_3458a(ag3458a):
+    ag3458a.acal.start_dcv()
+    ag3458a.acal.start_ohms()
+    ag3458a.utility.display = 'on'
 
 def ag3458a_high_accuracy(ag3458a):
-    ag3458a._is_high_speed = False
-    ag3458a._write('OCOMP OFF; DELAY 1; NDIG 9; NPLC 100')
+    ag3458a.is_high_speed = False
+    ag3458a.advanced.aperture_time = 100
 
 def ag3458a_high_speed(ag3458a):
-    ag3458a._is_high_speed = True
-    ag3458a._write('OCOMP OFF; DELAY 0; NPLC 1')
+    ag3458a.is_high_speed = True
+    ag3458a.advanced.aperture_time = 1
     time.sleep(3)
 
 def init_func():
     ag3458a_2 = ivi.agilent.agilent3458A("TCPIP::gpib1::gpib,20::INSTR",
             reset=True)
-    ag3458a_2._interface.timeout = 30
+    ag3458a_2._interface.timeout = 120
     ag3458a_2._is_high_speed = False
-    ag3458a_2._write('DISP ON')
+    ag3458a_2.utility.display = 'on'
     ag3458a_2.measurement_function = 'two_wire_resistance'
     ag3458a_2.auto_range = 'on'
     # ag3458a_2.range = 100e3
-    temp_2 = float(ag3458a_2._ask('TEMP?'))
+    temp_2 = ag3458a_2.utility.temp
     ag3458a_2.last_temp = datetime.datetime.utcnow()
     if DEBUG:
         ag3458a_2.last_acal = datetime.datetime.utcnow()
@@ -68,7 +57,9 @@ def init_func():
         ag3458a_2.last_acal_cal72 = 'test'
         #finish_acal_3458a(ag3458a_2)
     else:
-        acal_3458a(ag3458a_2, temp_2)
+        ag3458a_2.last_acal = datetime.datetime.utcnow()
+        ag3458a_2.last_acal_temp = temp_2
+        ag3458a_2.last_acal_cal72 = 'keep'
     return {'ag3458a_2': ag3458a_2 }
 
 def read_row(ag3458a_2):
@@ -79,21 +70,22 @@ def read_row(ag3458a_2):
     temp_2 = None
     if ((datetime.datetime.utcnow() - ag3458a_2.last_temp).total_seconds()
             > 15 * 60):
-        temp_2 = float(ag3458a_2._ask('TEMP?'))
+        temp_2 = ag3458a_2.utility.temp
         ag3458a_2.last_temp = datetime.datetime.utcnow()
         if ((datetime.datetime.utcnow() - ag3458a_2.last_acal).total_seconds() > 24 * 3600) \
                 or (abs(ag3458a_2.last_acal_temp - temp_2) >= 1):
             do_acal_3458a_2 = True
     if do_acal_3458a_2:
-        acal_3458a(ag3458a_2, temp_2)
+        acal_3458a(ag3458a_2)
     row['datetime'] = datetime.datetime.utcnow().isoformat()
     ag3458a_2.measurement.initiate()
-    if not ag3458a_2._is_high_speed:
+    if not ag3458a_2.is_high_speed:
         time.sleep(SAMPLE_INTERVAL)
     row['ag3458a_2_ohm'] = ag3458a_2.measurement.fetch(0)
     row['temp_2'] = temp_2
     row['last_acal_2'] = ag3458a_2.last_acal.isoformat()
     row['last_acal_2_cal72'] = ag3458a_2.last_acal_cal72
+    row['ag3458a_2_range'] = ag3458a_2.range
     return row
 
 
@@ -102,7 +94,7 @@ if __name__ == '__main__':
     readline.parse_and_bind('tab: self-insert')
 
     last_csvw_write = datetime.datetime(2018,1,1)
-    with open(OUTPUT_FILE, 'a') as csv_file:
+    with open(OUTPUT_FILE, 'a', newline='') as csv_file:
         initial_size = os.fstat(csv_file.fileno()).st_size
         csvw = csv.DictWriter(csv_file, fieldnames=FIELDNAMES)
         if initial_size == 0:
--- ks3458a-4w-res-log.py
+++ ks3458a-4w-res-log.py
@@ -9,15 +9,15 @@ import readline
 from enum import Enum, auto
 from quantiphy import Quantity
 
-OUTPUT_FILE = 'ks3458a-4w-res-log.csv'
-SAMPLE_INTERVAL = 10
+OUTPUT_FILE = 'ks3458a-4w-res-w-range-log.csv'
+sample_interval = 90
 FIELDNAMES = ('datetime', 'dut', 'dut_setting', 'ag3458a_2_ohm', 'temp_2', 'last_acal_2',
-        'last_acal_2_cal72')
+        'last_acal_2_cal72', 'ag3458a_2_range')
 WRITE_INTERVAL_SECONDS = 3600
 STABLE_THRESHOLD = 1e-3 # Should be stable within 0.1%
 STABLE_WAIT_TIME_SECONDS = 10
 
-DEBUG = True
+DEBUG = False
 
 class State(Enum):
     WAITING = auto()
@@ -26,29 +26,20 @@ class State(Enum):
 if DEBUG:
     WRITE_INTERVAL_SECONDS = 0
 
-def start_acal_3458a_dcv(ag3458a, temp):
-    ag3458a._write('ACAL DCV')
-    ag3458a.last_acal = datetime.datetime.utcnow()
-    ag3458a.last_acal_temp = temp
-
-def start_acal_3458a_ohms(ag3458a, temp):
-    ag3458a._write('ACAL OHMS')
-
-def finish_acal_3458a(ag3458a):
-    ag3458a.last_acal_cal72 = ag3458a._ask('CAL? 72').strip()
-
 def acal_3458a(ag3458a, temp):
-    start_acal_3458a_ohms(ag3458a, temp)
-    time.sleep(12*60)
-    finish_acal_3458a(ag3458a)
+    ag3458a.acal.start_dcv()
+    ag3458a.acal.start_ohms()
+    ag3458a.utility.display = 'on'
 
 def ag3458a_high_accuracy(ag3458a):
-    ag3458a._is_high_speed = False
-    ag3458a._write('OCOMP ON; DELAY 4; NDIG 9; NPLC 100')
+    ag3458a.is_high_speed = False
+    ag3458a.advanced.offset_compensation = 'on'
+    ag3458a.advanced.aperture_time = 100
 
 def ag3458a_high_speed(ag3458a):
-    ag3458a._is_high_speed = True
-    ag3458a._write('OCOMP OFF; DELAY 0; NPLC 1')
+    ag3458a.is_high_speed = True
+    ag3458a.advanced.offset_compensation = 'off'
+    ag3458a.advanced.aperture_time = 1
     time.sleep(3)
 
 def init_func():
@@ -56,11 +47,11 @@ def init_func():
             reset=True)
     ag3458a_2._interface.timeout = 120
     ag3458a_2._is_high_speed = False
-    ag3458a_2._write('DISP ON')
+    ag3458a_2.utility.display = 'on'
     ag3458a_2.measurement_function = 'four_wire_resistance'
     # ag3458a_2.auto_range = 'on'
-    ag3458a_2.range = 100e3
-    temp_2 = float(ag3458a_2._ask('TEMP?'))
+    ag3458a_2.range = 10e3
+    temp_2 = ag3458a_2.utility.temp
     ag3458a_2.last_temp = datetime.datetime.utcnow()
     if DEBUG:
         ag3458a_2.last_acal = datetime.datetime.utcnow()
@@ -68,7 +59,10 @@ def init_func():
         ag3458a_2.last_acal_cal72 = 'test'
         #finish_acal_3458a(ag3458a_2)
     else:
-        acal_3458a(ag3458a_2, temp_2)
+        ag3458a_2.last_acal = datetime.datetime.utcnow()
+        ag3458a_2.last_acal_temp = temp_2
+        ag3458a_2.last_acal_cal72 = 'keep'
+        # acal_3458a(ag3458a_2, temp_2)
     return {'ag3458a_2': ag3458a_2 }
 
 def read_row(ag3458a_2):
@@ -79,7 +73,7 @@ def read_row(ag3458a_2):
     temp_2 = None
     if ((datetime.datetime.utcnow() - ag3458a_2.last_temp).total_seconds()
             > 15 * 60):
-        temp_2 = float(ag3458a_2._ask('TEMP?'))
+        temp_2 = ag3458a_2.utility.temp
         ag3458a_2.last_temp = datetime.datetime.utcnow()
         if ((datetime.datetime.utcnow() - ag3458a_2.last_acal).total_seconds() > 24 * 3600) \
                 or (abs(ag3458a_2.last_acal_temp - temp_2) >= 1):
@@ -88,12 +82,13 @@ def read_row(ag3458a_2):
         acal_3458a(ag3458a_2, temp_2)
     row['datetime'] = datetime.datetime.utcnow().isoformat()
     ag3458a_2.measurement.initiate()
-    if not ag3458a_2._is_high_speed:
-        time.sleep(SAMPLE_INTERVAL)
+    if not ag3458a_2.is_high_speed:
+        time.sleep(sample_interval)
     row['ag3458a_2_ohm'] = ag3458a_2.measurement.fetch(0)
     row['temp_2'] = temp_2
     row['last_acal_2'] = ag3458a_2.last_acal.isoformat()
     row['last_acal_2_cal72'] = ag3458a_2.last_acal_cal72
+    row['ag3458a_2_range'] = ag3458a_2.range
     return row
 
 
@@ -102,7 +97,7 @@ if __name__ == '__main__':
     readline.parse_and_bind('tab: self-insert')
 
     last_csvw_write = datetime.datetime(2018, 1, 1)
-    with open(OUTPUT_FILE, 'a') as csv_file:
+    with open(OUTPUT_FILE, 'a', newline='') as csv_file:
         initial_size = os.fstat(csv_file.fileno()).st_size
         csvw = csv.DictWriter(csv_file, fieldnames=FIELDNAMES)
         if initial_size == 0:
